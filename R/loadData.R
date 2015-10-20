a9e659740ae83e28dd1d3e2dadf47df3b3d6342e
Dahaniel <daniel@muench.bio>
2015-10-20 14:26:40 +0200

    ran roxygen

--- R/loadData.R
+++ R/loadData.R
@@ -1,13 +1,15 @@
 #' loadData
 #' 
 #' load all DoOR.data
-#'
+#' 
+#' 
 #' @return attaches all DoOR data
 #' @author Daniel Münch <\email{daniel.muench@@uni-konstanz.de}>
-#' @export
-#'
 #' @examples
+#' 
 #' loadData()
+#' 
+#' @export loadData
 loadData <- function(){
   all.data <- data(package = "DoOR.data")
   data(list = all.data$results[,3])
