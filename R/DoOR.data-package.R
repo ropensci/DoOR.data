e3b8a360171ec00e11949d61855400d01ebdf17a
Dahaniel <daniel@muench.bio>
2015-10-20 14:21:48 +0200

    documented datasets

---
+++ R/DoOR.data-package.R
@@ -0,0 +1,1381 @@
+
+
+#' ab2B
+#' 
+#' DoOR response data
+#' 
+#' @name ab2B
+#' @docType data
+#' @format A data frame with 693 observations on 12 variables.
+#' @keywords datasets
+
+NULL
+
+
+
+
+
+#' ab4B
+#' 
+#' DoOR response data
+#' 
+#' @name ab4B
+#' @docType data
+#' @format A data frame with 693 observations on 10 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' ab5B
+#' 
+#' DoOR response data
+#' 
+#' @name ab5B
+#' @docType data
+#' @format A data frame with 693 observations on 9 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' ac1A
+#' 
+#' DoOR response data
+#' 
+#' @name ac1A
+#' @docType data
+#' @format A data frame with 693 observations on 7 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' ac1BC
+#' 
+#' DoOR response data
+#' 
+#' pooled measurements of ac1B and ac1C OSNs
+#' 
+#' @name ac1BC
+#' @docType data
+#' @format A data frame with 693 observations on 6 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' ac1B
+#' 
+#' DoOR response data
+#' 
+#' @name ac1B
+#' @docType data
+#' @format A data frame with 693 observations on 6 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' ac1
+#' 
+#' DoOR response data
+#' 
+#' pooled measurements of the ac1 sensillum
+#' 
+#' @name ac1
+#' @docType data
+#' @format A data frame with 693 observations on 6 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' ac2A
+#' 
+#' DoOR response data
+#' 
+#' @name ac2A
+#' @docType data
+#' @format A data frame with 693 observations on 7 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' ac2BC
+#' 
+#' DoOR response data
+#' 
+#' pooled measurements of ac2B and ac2C OSNs
+#' 
+#' @name ac2BC
+#' @docType data
+#' @format A data frame with 693 observations on 6 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' ac2B
+#' 
+#' DoOR response data
+#' 
+#' @name ac2B
+#' @docType data
+#' @format A data frame with 693 observations on 6 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' ac2
+#' 
+#' DoOR response data
+#' 
+#' pooled measurements of the ac2 sensilum
+#' 
+#' @name ac2
+#' @docType data
+#' @format A data frame with 693 observations on 6 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' ac3A
+#' 
+#' DoOR response data
+#' 
+#' @name ac3A
+#' @docType data
+#' @format A data frame with 693 observations on 8 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' ac3B
+#' 
+#' DoOR response data
+#' 
+#' @name ac3B
+#' @docType data
+#' @format A data frame with 693 observations on 9 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' ac3_noOr35a
+#' 
+#' DoOR response data
+#' 
+#' mpooled measurements of the ac3 sensillum with Or35a silenced
+#' 
+#' @name ac3_noOr35a
+#' @docType data
+#' @format A data frame with 693 observations on 6 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' ac4
+#' 
+#' DoOR response data
+#' 
+#' pooled measurements of the ac4 sensillum
+#' 
+#' @name ac4
+#' @docType data
+#' @format A data frame with 693 observations on 8 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' AL.map
+#' 
+#' The AL map after Grabe et al.
+#' 
+#' @name AL.map
+#' @docType data
+#' @format The format is: List of 5 $ glomeruli :'data.frame': 19834 obs. of 3
+#' variables: ..$ glomerulus: Factor w/ 51 levels "D","DA1","DA2",..: 4 4 4 4 4
+#' 4 4 4 4 4 ...  ..$ x : num [1:19834] 33.1 33.1 33.1 33.1 33.1 ...  ..$ y :
+#' num [1:19834] 24.2 24.2 24.3 24.4 24.4 ...  $ unmapped_not.olf:'data.frame':
+#' 1274 obs. of 3 variables: ..$ glomerulus: Factor w/ 3 levels
+#' "VA7m","VP2","VP3": 3 3 3 3 3 3 3 3 3 3 ...  ..$ x : num [1:1274] 280 279
+#' 279 279 279 ...  ..$ y : num [1:1274] 33.8 33.8 33.9 33.9 34 ...  $
+#' background :'data.frame': 9428 obs. of 3 variables: ..$ group: Factor w/ 4
+#' levels "slice1","slice2",..: 4 4 4 4 4 4 4 4 4 4 ...  ..$ x : num [1:9428]
+#' 39 38.9 38.8 38.7 38.6 ...  ..$ y : num [1:9428] 62.2 62.2 62.2 62.1 62.1
+#' ...  $ bg.cutout :'data.frame': 2361 obs. of 3 variables: ..$ group: Factor
+#' w/ 5 levels "c1","c2","c3",..: 5 5 5 5 5 5 5 5 5 5 ...  ..$ x : num [1:2361]
+#' 99.1 99.3 99.5 99.6 99.8 ...  ..$ y : num [1:2361] 15.6 15.6 15.6 15.6 15.7
+#' ...  $ labels :'data.frame': 54 obs. of 3 variables: ..$ glomerulus: Factor
+#' w/ 54 levels "D","DA1","DA2",..: 34 27 12 5 32 3 52 51 54 53 ...  ..$ x :
+#' num [1:54] 157 105 110 31 25 ...  ..$ y : num [1:54] 51.9 55 15 35 41 ...
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' data.format
+#' 
+#' DoOR response data template
+#' 
+#' @name data.format
+#' @docType data
+#' @format A data frame with 693 observations on 5 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' dataset.info
+#' 
+#' information on the studies that contributed to DoOR
+#' 
+#' @name dataset.info
+#' @docType data
+#' @format A data frame with 42 observations on 15 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' DoOR.mappings
+#' 
+#' the mappings of DoOR responding units to the correesponding morphological structures
+#' 
+#' @name DoOR.mappings
+#' @docType data
+#' @format A data frame with 96 observations on 20 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' excluded.data
+#' 
+#' datasets that were excluded during the merging process
+#' 
+#' @name excluded.data
+#' @docType data
+#' @format A data frame with 78 observations on 2 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' glo.dist
+#' 
+#' glomerular distances
+#' 
+#' @name glo.dist
+#' @docType data
+#' @format A data frame with 49 observations on 49 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Gr21a.Gr63a
+#' 
+#' DoOR response data
+#' 
+#' data measured from the CO2 responsive sensillum
+#' 
+#' @name Gr21a.Gr63a
+#' @docType data
+#' @format A data frame with 693 observations on 10 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Ir31a
+#' 
+#' DoOR response data
+#' 
+#' @name Ir31a
+#' @docType data
+#' @format A data frame with 693 observations on 6 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Ir41a
+#' 
+#' DoOR response data
+#' 
+#' @name Ir41a
+#' @docType data
+#' @format A data frame with 693 observations on 6 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Ir64a.DC4
+#' 
+#' DoOR response data
+#' 
+#' measurements from the Ir64a expressing OSNs that project to DC4
+#' 
+#' @name Ir64a.DC4
+#' @docType data
+#' @format A data frame with 693 observations on 6 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Ir64a.DP1m
+#' 
+#' DoOR response data
+#' 
+#' measurements from the Ir64a expressing OSNs that project to DP1m
+#' 
+#' @name Ir64a.DP1m
+#' @docType data
+#' @format A data frame with 693 observations on 6 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Ir75a
+#' 
+#' DoOR response data
+#' 
+#' @name Ir75a
+#' @docType data
+#' @format A data frame with 693 observations on 7 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Ir75d
+#' 
+#' DoOR response data
+#' 
+#' @name Ir75d
+#' @docType data
+#' @format A data frame with 693 observations on 6 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Ir76a
+#' 
+#' DoOR response data
+#' 
+#' @name Ir76a
+#' @docType data
+#' @format A data frame with 693 observations on 6 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Ir84a
+#' 
+#' DoOR response data
+#' 
+#' @name Ir84a
+#' @docType data
+#' @format A data frame with 693 observations on 7 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Ir92a
+#' 
+#' DoOR response data
+#' 
+#' @name Ir92a
+#' @docType data
+#' @format A data frame with 693 observations on 6 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' odor
+#' 
+#' data.frame containing information on the odors in DoOR
+#' 
+#' @name odor
+#' @docType data
+#' @format A data frame with 693 observations on 22 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#'Or10a
+#' 
+#' DoOR response data
+#' 
+#' @name Or10a
+#' @docType data
+#' @format A data frame with 693 observations on 15 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Or13a
+#' 
+#' DoOR response data
+#' 
+#' @name Or13a
+#' @docType data
+#' @format A data frame with 693 observations on 12 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Or19a
+#' 
+#' DoOR response data
+#' 
+#' @name Or19a
+#' @docType data
+#' @format A data frame with 693 observations on 9 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Or1a
+#' 
+#' DoOR response data
+#' 
+#' @name Or1a
+#' @docType data
+#' @format A data frame with 693 observations on 6 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Or22a
+#' 
+#' DoOR response data
+#' 
+#' @name Or22a
+#' @docType data
+#' @format A data frame with 693 observations on 19 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Or22b
+#' 
+#' DoOR response data
+#' 
+#' @name Or22b
+#' @docType data
+#' @format A data frame with 693 observations on 6 variables.
+#' @keywords datasets
+#'
+NULL
+
+
+#' Or22c
+#' 
+#' DoOR response data
+#' 
+#' @name Or22c
+#' @docType data
+#' @format A data frame with 693 observations on 7 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Or23a
+#' 
+#' DoOR response data
+#' 
+#' @name Or23a
+#' @docType data
+#' @format A data frame with 693 observations on 7 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Or24a
+#' 
+#' DoOR response data
+#' 
+#' @name Or24a
+#' @docType data
+#' @format A data frame with 693 observations on 7 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Or2a
+#' 
+#' DoOR response data
+#' 
+#' @name Or2a
+#' @docType data
+#' @format A data frame with 693 observations on 9 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Or30a
+#' 
+#' DoOR response data
+#' 
+#' @name Or30a
+#' @docType data
+#' @format A data frame with 693 observations on 8 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Or33a
+#' 
+#' DoOR response data
+#' 
+#' @name Or33a
+#' @docType data
+#' @format A data frame with 693 observations on 6 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Or33b
+#' 
+#' DoOR response data
+#' 
+#' @name Or33b
+#' @docType data
+#' @format A data frame with 693 observations on 9 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Or33c
+#' 
+#' DoOR response data
+#' 
+#' @name Or33c
+#' @docType data
+#' @format A data frame with 693 observations on 6 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Or35a
+#' 
+#' DoOR response data
+#' 
+#' @name Or35a
+#' @docType data
+#' @format A data frame with 693 observations on 9 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Or42a
+#' 
+#' DoOR response data
+#' 
+#' @name Or42a
+#' @docType data
+#' @format A data frame with 693 observations on 11 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Or42b
+#' 
+#' DoOR response data
+#' 
+#' @name Or42b
+#' @docType data
+#' @format A data frame with 693 observations on 14 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Or43a
+#' 
+#' DoOR response data
+#' 
+#' @name Or43a
+#' @docType data
+#' @format A data frame with 693 observations on 7 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Or43b
+#' 
+#' DoOR response data
+#' 
+#' @name Or43b
+#' @docType data
+#' @format A data frame with 693 observations on 10 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Or45a
+#' 
+#' DoOR response data
+#' 
+#' @name Or45a
+#' @docType data
+#' @format A data frame with 693 observations on 8 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Or45b
+#' 
+#' DoOR response data
+#' 
+#' @name Or45b
+#' @docType data
+#' @format A data frame with 693 observations on 8 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Or46a
+#' 
+#' DoOR response data
+#' 
+#' @name Or46a
+#' @docType data
+#' @format A data frame with 693 observations on 8 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Or47a
+#' 
+#' DoOR response data
+#' 
+#' @name Or47a
+#' @docType data
+#' @format A data frame with 693 observations on 11 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Or47b
+#' 
+#' DoOR response data
+#' 
+#' @name Or47b
+#' @docType data
+#' @format A data frame with 693 observations on 11 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Or49a
+#' 
+#' DoOR response data
+#' 
+#' @name Or49a
+#' @docType data
+#' @format A data frame with 693 observations on 8 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Or49b
+#' 
+#' DoOR response data
+#' 
+#' @name Or49b
+#' @docType data
+#' @format A data frame with 693 observations on 11 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Or59a
+#' 
+#' DoOR response data
+#' 
+#' @name Or59a
+#' @docType data
+#' @format A data frame with 693 observations on 8 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Or59b
+#' 
+#' DoOR response data
+#' 
+#' @name Or59b
+#' @docType data
+#' @format A data frame with 693 observations on 15 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Or59c
+#' 
+#' DoOR response data
+#' 
+#' @name Or59c
+#' @docType data
+#' @format A data frame with 693 observations on 9 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Or65a
+#' 
+#' DoOR response data
+#' 
+#' @name Or65a
+#' @docType data
+#' @format A data frame with 693 observations on 9 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Or67a
+#' 
+#' DoOR response data
+#' 
+#' @name Or67a
+#' @docType data
+#' @format A data frame with 693 observations on 8 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Or67b
+#' 
+#' DoOR response data
+#' 
+#' @name Or67b
+#' @docType data
+#' @format A data frame with 693 observations on 9 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Or67c
+#' 
+#' DoOR response data
+#' 
+#' @name Or67c
+#' @docType data
+#' @format A data frame with 693 observations on 10 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Or67d
+#' 
+#' DoOR response data
+#' 
+#' @name Or67d
+#' @docType data
+#' @format A data frame with 693 observations on 7 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Or69a
+#' 
+#' DoOR response data
+#' 
+#' @name Or69a
+#' @docType data
+#' @format A data frame with 693 observations on 6 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Or71a
+#' 
+#' DoOR response data
+#' 
+#' @name Or71a
+#' @docType data
+#' @format A data frame with 693 observations on 9 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Or74a
+#' 
+#' DoOR response data
+#' 
+#' @name Or74a
+#' @docType data
+#' @format A data frame with 693 observations on 8 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Or7a
+#' 
+#' DoOR response data
+#' 
+#' @name Or7a
+#' @docType data
+#' @format A data frame with 693 observations on 16 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Or82a
+#' 
+#' DoOR response data
+#' 
+#' @name Or82a
+#' @docType data
+#' @format A data frame with 693 observations on 14 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Or83c
+#' 
+#' DoOR response data
+#' 
+#' @name Or83c
+#' @docType data
+#' @format A data frame with 693 observations on 6 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Or85a
+#' 
+#' DoOR response data
+#' 
+#' @name Or85a
+#' @docType data
+#' @format A data frame with 693 observations on 7 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Or85b
+#' 
+#' DoOR response data
+#' 
+#' @name Or85b
+#' @docType data
+#' @format A data frame with 693 observations on 13 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Or85c
+#' 
+#' DoOR response data
+#' 
+#' @name Or85c
+#' @docType data
+#' @format A data frame with 693 observations on 8 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Or85d
+#' 
+#' DoOR response data
+#' 
+#' @name Or85d
+#' @docType data
+#' @format A data frame with 693 observations on 8 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Or85e
+#' 
+#' DoOR response data
+#' 
+#' @name Or85e
+#' @docType data
+#' @format A data frame with 693 observations on 6 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Or85f
+#' 
+#' DoOR response data
+#' 
+#' @name Or85f
+#' @docType data
+#' @format A data frame with 693 observations on 7 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Or88a
+#' 
+#' DoOR response data
+#' 
+#' @name Or88a
+#' @docType data
+#' @format A data frame with 693 observations on 8 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Or92a
+#' 
+#' DoOR response data
+#' 
+#' @name Or92a
+#' @docType data
+#' @format A data frame with 693 observations on 11 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Or94a
+#' 
+#' DoOR response data
+#' 
+#' @name Or94a
+#' @docType data
+#' @format A data frame with 693 observations on 9 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Or94b
+#' 
+#' DoOR response data
+#' 
+#' @name Or94b
+#' @docType data
+#' @format A data frame with 693 observations on 9 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+#' Or98a
+#' 
+#' DoOR response data
+#' 
+#' @name Or98a
+#' @docType data
+#' @format A data frame with 693 observations on 11 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' Or9a
+#' 
+#' DoOR response data
+#' 
+#' @name Or9a
+#' @docType data
+#' @format A data frame with 693 observations on 8 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' ORs
+#' 
+#' list of responding units in DoOR
+#' 
+#' including information on whether a unitresponding unit is expressed in adult (0), lava (1) or both (2)
+#' 
+#' @name ORs
+#' @docType data
+#' @format A data frame with 78 observations on 2 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' pb2A
+#' 
+#' DoOR response data
+#' 
+#' @name pb2A
+#' @docType data
+#' @format A data frame with 693 observations on 8 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' non normalized DoOR response matrix
+#' 
+#' precomputed DoOR response matrix with no global normalization applied
+#' 
+#' @name response.matrix_non.normalized
+#' @docType data
+#' @format A data frame with 693 observations on 78 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' DoOR response matrix
+#' 
+#' globally normalized, precomputed DoOR response matrix
+#' 
+#' @name response.matrix
+#' @docType data
+#' @format A data frame with 693 observations on 78 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' response.range
+#' 
+#' the response range of the studies included in DoOR
+#' 
+#' @name response.range
+#' @docType data
+#' @format A data frame with 42 observations on 4 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
+
+
+#' weight.globNorm
+#' 
+#' matrix of studies vs responding units, used for calculating the weight for global normalization 
+#' 
+#' @name weight.globNorm
+#' @docType data
+#' @format A data frame with 78 observations on 42 variables.
+#' @keywords datasets
+#' 
+NULL
+
+
+
