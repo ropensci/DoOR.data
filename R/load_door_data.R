589804e39e09d3af3efd583d60a117790b22d6d0
Dahaniel <daniel@muench.bio>
2017-03-07 15:57:45 +0100

    Added possibility to suppress user interaction when loading data as otherwise e.g. the vignettes would not build.

--- R/load_door_data.R
+++ R/load_door_data.R
@@ -3,6 +3,9 @@
 #' load all DoOR.data
 #' 
 #' 
+#' @param nointeraction if set to TRUE does not promt security message. Necessary e.g. for building
+#'   vignettes during CHECK.
+#'
 #' @return attaches all DoOR data to the main workspace
 #' @aliases loadData load_door_data
 #' @author Daniel Münch <\email{daniel.muench@uni-konstanz.de}>
@@ -15,13 +18,15 @@
 #' # it is also possible to load individual data sets using data().
 #' data(Or22a)
 #' @export load_door_data
-load_door_data <- function(){
+load_door_data <- function(nointeraction = FALSE){
   
-  answer <- ""
-  while(answer != "n" & answer != "y") { 
-    answer <- readline("This will load all DoOR data into the current environment, possibly overwriting existing data. Continue? (y/n)? ")
-    if(answer == "y") break()
-    if(answer == "n") stop("aborted loading data")
+  if(nointeraction == FALSE){
+    answer <- ""
+    while(answer != "n" & answer != "y") { 
+      answer <- readline("This will load all DoOR data into the current environment, possibly overwriting existing data. Continue? (y/n)? ")
+      if(answer == "y") break()
+      if(answer == "n") stop("aborted loading data")
+    }
   }
   
   
