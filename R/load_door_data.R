576749473fc73284e4ff84b67c6d8fdbe0163fd5
Dahaniel <daniel@muench.bio>
2017-10-09 11:25:38 +0100

    shorted lines

--- R/load_door_data.R
+++ R/load_door_data.R
@@ -3,9 +3,9 @@
 #' load all DoOR.data
 #' 
 #' 
-#' @param nointeraction if set to TRUE does not promt security message. Necessary e.g. for building
-#'   vignettes during CHECK.
-#'
+#' @param nointeraction if set to TRUE does not promt security message.
+#'   Necessary e.g. for building vignettes during CHECK.
+#'   
 #' @return attaches all DoOR data to the main workspace
 #' @aliases loadData load_door_data
 #' @author Daniel Münch <\email{daniel.muench@uni-konstanz.de}>
@@ -18,18 +18,23 @@
 #' # it is also possible to load individual data sets using data().
 #' data(Or22a)
 #' @export load_door_data
-load_door_data <- function(nointeraction = FALSE){
-  
-  if(nointeraction == FALSE){
+load_door_data <- function(nointeraction = FALSE) {
+  if (nointeraction == FALSE) {
     answer <- ""
-    while(answer != "n" & answer != "y") { 
-      answer <- readline("This will load all DoOR data into the current environment, possibly overwriting existing data. Continue? (y/n)? ")
-      if(answer == "y") break()
-      if(answer == "n") stop("aborted loading data")
+    while (answer != "n" & answer != "y") {
+      answer <-
+        readline(
+          "This will load all DoOR data into the current environment, possibly 
+          overwriting existing data. Continue? (y/n)? "
+        )
+      if (answer == "y")
+        break()
+      if (answer == "n")
+        stop("aborted loading data")
     }
   }
   
   
   all.data <- utils::data(package = "DoOR.data")
-  utils::data(list = all.data$results[,3])
+  utils::data(list = all.data$results[, 3])
 }
--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,16 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage(paste0("\nWelcome to ", utils::packageDescription(pkgname)$Package,
-                               "\nVersion: ",   utils::packageDescription(pkgname)$Version,
-                               "\nReleased: ",  utils::packageDescription(pkgname)$Date,
-                               "\n\nPlease use load_door_data() to load all data into your workspace now.\n"
-  ))
+  packageStartupMessage(
+    paste0(
+      "\nWelcome to ",
+      utils::packageDescription(pkgname)$Package,
+      "\nVersion: ",
+      utils::packageDescription(pkgname)$Version,
+      "\nReleased: ",
+      utils::packageDescription(pkgname)$Date,
+      "\n\nPlease use load_door_data() to load all data into your workspace 
+      now.\n"
+    )
+  )
 }
 
 # transition to new function names  ---------------------------------------
